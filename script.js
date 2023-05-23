5e49184fd4fa99daa2c556741b763a3b04aed853
vickneee <victoria.vavulina@outlook.com>
2023-05-23 22:50:30 +0300

    Comments corr

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 const timeEl = document.querySelector('.time');
-// const toggle = document.querySelector('.toggle');
+const toggle = document.querySelector('.toggle');
+// const dateEl = document.querySelector('.date')
 
 // const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 // const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
