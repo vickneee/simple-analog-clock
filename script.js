188f09b654037f3f714b6281b5c9bb8c83f1cab4
vickneee <victoria.vavulina@outlook.com>
2023-05-23 20:50:39 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+const secondEl = document.querySelector('.second');
+const timeEl = document.querySelector('.time');
+const dateEl = document.querySelector('.date');
+const toggle = document.querySelector('.toggle');
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+toggle.addEventListener('click', (e) => {
+    const html = document.querySelector('html');
+    if (html.classList.contains('dark')) {
+        html.classList.remove('dark');
+        e.target.innerHTML = 'Dark mode';
+    } else {
+        html.classList.add('dark');
+        e.target.innerHTML = 'Light mode';
+    }
+});
+
+function setTime() {
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const hours = time.getHours();
+    const hoursForClock = hours % 24;
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+
+    const hourRotation = hoursForClock * 30 + minutes * 0.5; // Calculate the rotation for the hour hand
+    const minuteRotation = minutes * 6 + seconds * 0.1; // Calculate the rotation for the minute hand
+    const secondRotation = seconds * 6; // Calculate the rotation for the second hand
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${hourRotation}deg)`; // Apply rotation to the hour hand
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${minuteRotation}deg)`; // Apply rotation to the minute hand
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${secondRotation}deg)`; // Apply rotation to the second hand
+
+    timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes}`; // Display the current time
+    dateEl.innerHTML = `${days[day]}, ${months[month]} ${time.getDate()}`; // Display the current date
+}
+
+setInterval(setTime, 1000); // Update the clock every second
